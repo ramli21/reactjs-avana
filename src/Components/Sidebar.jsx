5e369b63dc990409dbd29ace828c83b2d11e9386
2nd_EA <ahmad_ramli@outlook.com>
2020-12-17 10:03:50 +0700

    action show hide menu

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,82 +6,102 @@ import { connect } from "react-redux";
 import { getMenuItem, getMenuToggle } from '../redux/selectors';
 import { toggleSidebar, toggleDropdown } from '../redux/actions';
 
-const Sidebar = props => {
-  function toggleMenu(e) {
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...props };
+
+    this.toggleSidebar = () => props.toggleSidebar();
+  }
+
+  toggleMenu = (e) => {
     e.preventDefault();
 
-    props.toggleSidebar()
+    this.toggleSidebar()
   }
 
-  const MenuItem = ({ idx, item, path }) => {
-    return (
-      <ListGroup.Item active={idx === 0} action href={path} disabled={!item.isAllowed} >
-        <span className="label">{item.id}</span>
-      </ListGroup.Item>
-    )
-  }
+  render() {
+    const MenuChilds = ({ parent }) => {
+      const showDropdown = () => {
+        this.props.toggleDropdown(parent)
 
-  const MenuChilds = ({ parent }) => {
-    const showDropdown = () => {
-      props.toggleDropdown(parent)
-    }
-  
-    return (
-      <ListGroup.Item className={`dropdown ${parent.isAllowed ? "show":""}`} onClick={showDropdown}>
-        <span className="label parent">{parent.id}</span>
-  
-        <ListGroup>
-          {parent.childs && parent.childs.length
-            ? parent.childs.map((item, index) => {
-              return <MenuItem key={`menu-${index}`} item={item} path="/" idx={index + 1} />
-            })
-            : ""
-          }
-        </ListGroup>
-      </ListGroup.Item>
-    )
-  }
-
-  return (
-    <div id="sidebar" className={`${props.is_hidden ? "hide" : ""}`}>
-      <div className="logo">
-        <img src="https://www.avana.id/assets/images/logo.webp" alt="avana-logo" />
-      </div>
-      <div className="user-avatar text-center">
-        <img src={avatar} alt="Avatar" width="86" />
-        <h5 className="text-center text-white mt-3">Avriza Bramantyo</h5>
-      </div>
-
-      <div className="navigation">
-        <ListGroup>
-          {props.menus && props.menus.length
-            ? props.menus.map((item, index) => {
-              if(item.isShowed) {
-                if (item.childs) {
-                  return <MenuChilds key={item.id} parent={item} />
-                } else {
-                  return <MenuItem key={`menu-${index}`} item={item} path="/" idx={index} />
-                }
-              }
-            })
-            : ""
-          }
-        </ListGroup>
-      </div>
-
-      {props.is_hidden
-        ? <button className="toggle" onClick={toggleMenu}>&gt;</button>
-        : <button className="toggle" onClick={toggleMenu}>&lt;</button>
-
+        this.forceUpdate()
       }
-    </div>
-  )
+
+      return (
+        <ListGroup.Item className={`dropdown ${parent.isAllowed ? "show" : ""}`}>
+          <span className="label parent" onClick={showDropdown}>{parent.id}</span>
+
+          <ListGroup>
+            {parent.childs && parent.childs.length
+              ? parent.childs.map((item, index) => {
+                if (item.isShowed) {
+                  if (item.childs) {
+                    return <MenuChilds key={item.id} parent={item} />
+                  } else {
+                    return <MenuItem key={`menu-${index}`} item={item} path="/" idx={index + 1} is_child={true} />
+                  }
+                } else {
+                  return null
+                }
+              })
+              : ""
+            }
+          </ListGroup>
+        </ListGroup.Item>
+      )
+    }
+
+    const MenuItem = ({ idx, item, path, is_child }) => {
+      return (
+        <ListGroup.Item active={idx === 0} action href={path} disabled={!item.isAllowed} className={is_child ? "child" : ""}>
+          <span className="label">{item.id}</span>
+        </ListGroup.Item>
+      )
+    }
+
+    return (
+      <div id="sidebar" className={`${this.props.is_hidden ? "hide" : ""}`}>
+        <div className="logo">
+          <img src="https://www.avana.id/assets/images/logo.webp" alt="avana-logo" />
+        </div>
+        <div className="user-avatar text-center">
+          <img src={avatar} alt="Avatar" width="86" />
+          <h5 className="text-center text-white mt-3">Avriza Bramantyo</h5>
+        </div>
+
+        <div className="navigation">
+          <ListGroup>
+            {this.props.menus && this.props.menus.length
+              ? this.props.menus.map((item, index) => {
+                if (item.isShowed) {
+                  if (item.childs) {
+                    return <MenuChilds key={item.id} parent={item} />
+                  } else {
+                    return <MenuItem key={`menu-${index}`} item={item} path="/" idx={index} />
+                  }
+                } else {
+                  return null
+                }
+              })
+              : null
+            }
+          </ListGroup>
+        </div>
+
+        {this.props.is_hidden
+          ? <button className="toggle" onClick={this.toggleMenu}>&gt;</button>
+          : <button className="toggle" onClick={this.toggleMenu}>&lt;</button>
+        }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   const menus = getMenuItem(state);
   const is_hidden = getMenuToggle(state);
-  
+
   return { menus: menus, is_hidden: is_hidden }
 }
 
--- src/redux/reducers/menu.js
+++ src/redux/reducers/menu.js
@@ -1,56 +1,87 @@
 const menus = require('../../utils/menus.json');
 
 const initialState = {
-	items: [
-		{
-			"id": "dashboard",
-			"isShowed": true,
-			"isAllowed": true
-		},
-		{
-			"id": "hq",
-			"isShowed": false,
-			"isAllowed": false,
-			"childs": [
-				{
-					"id": "hq_stockist",
-					"isShowed": false,
-					"isAllowed": false
-				},
-				{
-					"id": "hq_dropship_affiliate",
-					"isShowed": false,
-					"isAllowed": false
-				}
-			]
-		},
-		{
-			"id": "agent",
-			"isShowed": true,
-			"isAllowed": false,
-			"childs": [
-				{
-					"id": "my_purchase",
-					"isShowed": true,
-					"isAllowed": true
-				}
-			]
-		},
-	],
+	items: menus,
 	is_hidden: false
 };
+
+function findKeys(state, id) {
+	let keys = [];
+
+	for (let i = 0; i < state.items.length; i++) {
+		if (keys.length !== 0) break;
+
+		const menu_1 = state.items[i];
+		if (menu_1.childs) {
+			for (let a = 0; a < menu_1.childs.length; a++) {
+				const menu_2 = menu_1.childs[a];
+				if (menu_2.id === id) {
+					keys = [i, a];
+
+					break;
+				} else {
+					if (menu_2.childs) {
+						for (let x = 0; x < menu_2.childs.length; x++) {
+							const menu_3 = menu_2.childs[x]
+
+							if (menu_3.id === id) {
+								keys = [i, a, x];
+
+								break;
+							} else {
+								if (menu_3.childs) {
+									for (let y = 0; y < menu_3.childs; y++) {
+										const menu_4 = menu_3.childs;
+
+										if (menu_4.id === id) {
+											keys = [i, a, x, y];
+
+											break;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return keys;
+}
 
 export default function Menu(state = initialState, action) {
 	switch (action.type) {
 		case "TOGGLE_MENU":
 			const { id, isAllowed } = action.payload.content;
 			const idx = state.items.findIndex((obj => obj.id === id))
-			console.log("this is the result ", state)
-			
-			state.items[idx] = {
-				...state.items[idx],
-				isAllowed: !isAllowed
-			};
+
+			if (idx === -1) {
+				let keys = findKeys(state, id)
+
+				if (keys.length === 2) {
+					state.items[keys[0]].childs[keys[1]] = {
+						...state.items[keys[0]].childs[keys[1]],
+						isAllowed: !isAllowed
+					}
+				} else if (keys.length === 3) {
+					state.items[keys[0]].childs[keys[1]].childs[keys[2]] = {
+						...state.items[keys[0]].childs[keys[1]].childs[keys[2]],
+						isAllowed: !isAllowed
+					}
+				} else if (keys.length === 4) {
+					state.items[keys[0]].childs[keys[1]].childs[keys[2]].childs[keys[3]] = {
+						...state.items[keys[0]].childs[keys[1]].childs[keys[2]].childs[keys[3]],
+						isAllowed: !isAllowed
+					}
+				}
+			} else {
+				state.items[idx] = {
+					...state.items[idx],
+					isAllowed: !isAllowed
+				};
+			}
 
 
 			return {
